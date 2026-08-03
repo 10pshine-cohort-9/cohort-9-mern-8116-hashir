6401c5461a570ece8ab0aaf19d5aad5bbab089b7
HashirUmrani <hashir.muhkam@gmail.com>
2026-08-04 00:33:36 +0500

    implemented authentication and authorization

---
+++ backend/src/config/database.js
@@ -0,0 +1,14 @@
+const { model } = require("mongoose");
+
+const mongoose = require("mongoose");
+
+async function connectToDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to database");
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+module.exports = connectToDB;
