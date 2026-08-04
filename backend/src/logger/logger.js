233ddeafbc8a077d5708ab96bca732904b428493
HashirUmrani <hashir.muhkam@gmail.com>
2026-08-04 20:08:26 +0500

    fix coderabbit pointout issue

--- backend/src/logger/logger.js
+++ backend/src/logger/logger.js
@@ -1,15 +1,16 @@
 const pino = require("pino");
 
-const logger = pino({
-  level: "info",
-  transport: {
-    target: "pino-pretty",
-    options: {
-      colorize: true,
-      translateTime: "SYS:standard",
-      ignore: "pid,hostname",
-    },
-  },
-});
+const logger =
+  process.env.NODE_ENV === "production"
+    ? pino()
+    : pino({
+        transport: {
+          target: "pino-pretty",
+          options: {
+            colorize: true,
+            translateTime: "SYS:standard",
+          },
+        },
+      });
 
 module.exports = logger;
